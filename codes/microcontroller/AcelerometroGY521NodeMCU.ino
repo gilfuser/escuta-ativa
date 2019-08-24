d7c6ae7a58dce7c4418bc8d6f95c6ff4b75b6133
grazilaut <grazilaut@users.noreply.github.com>
2019-08-24 19:29:18 -0300

    Create AcelerometroGY521NodeMCU.ino
    Leitura de dados do acelerômetro (mod. GY521) pela placa NodeMCU e envio via OSC.

---
+++ codes/microcontroller/AcelerometroGY521NodeMCU.ino
@@ -0,0 +1,114 @@
+
+#include <ESP8266WiFi.h> //INCLUSÃO DA BIBLIOTECA NECESSÁRIA PARA FUNCIONAMENTO DO CÓDIGO
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+
+const char* ssid = "NET_2.4G_Comunidade azul"; //VARIÁVEL QUE ARMAZENA O NOME DA REDE SEM FIO EM QUE VAI CONECTAR
+const char* password = "dunadeareia"; //VARIÁVEL QUE ARMAZENA A SENHA DA REDE SEM FIO EM QUE VAI CONECTAR
+
+WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
+const IPAddress outIp(192,168,0,14);        // remote IP of your computer
+const unsigned int outPort = 57120;         // remote port to receive OSC
+//const unsigned int localPort = 8888;      // local port to listen for OSC packets (actually not used for sending)
+
+//DEFINIÇÃO DE IP FIXO PARA O NODEMCU
+//IPAddress ip(192,168,0,175); //COLOQUE UMA FAIXA DE IP DISPONÍVEL DO SEU ROTEADOR. EX: 192.168.1.110 **** ISSO VARIA, NO MEU CASO É: 192.168.0.175
+//IPAddress gateway(192,168,0,1); //GATEWAY DE CONEXÃO (ALTERE PARA O GATEWAY DO SEU ROTEADOR)
+//IPAddress subnet(255,255,255,0); //MASCARA DE REDE
+//WiFiServer server(80); //CASO OCORRA PROBLEMAS COM A PORTA 80, UTILIZE OUTRA (EX:8082,8089) E A CHAMADA DA URL FIC
+
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+//int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+//int16_t temperature; // variables for temperature data
+char tmp_str[7]; // temporary variable used in convert function
+/*
+ char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+ */
+ 
+void setup() {
+  delay(10);
+  Serial.begin(9600);
+ 
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
+// Connect to WiFi network
+    WiFi.mode(WIFI_STA);
+    WiFi.hostname("esposc");
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    OSCMessage msg("/espInit");
+    Udp.beginPacket(outIp, outPort);
+    msg.send(Udp);
+    Udp.endPacket();
+    msg.empty();
+    
+    Serial.println("");
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("Starting UDP");
+    //Udp.begin(localPort);
+    //Serial.print("Local port: ");
+    //Serial.println(Udp.localPort());
+    delay(20000);
+}
+
+void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  //temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  //gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  //gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  //gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+  Serial.print(accelerometer_x);
+  Serial.print("x");
+  Serial.print(accelerometer_y);
+  Serial.print("y");
+  Serial.print(accelerometer_z);
+  Serial.print("z");
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  // Serial.print("t"); Serial.print(temperature/340.00+36.53);
+  // Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  // Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  // Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  // Serial.println();
+  // delay(1000);
+
+OSCMessage msg("/espData");
+    msg.add(accelerometer_x);
+    msg.add(accelerometer_y);
+    msg.add(accelerometer_z);
+    Udp.beginPacket(outIp, outPort);
+    msg.send(Udp);
+    Udp.endPacket();
+    msg.empty();
+    delay(10);
+}
