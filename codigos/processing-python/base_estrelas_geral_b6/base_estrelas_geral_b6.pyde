1fb3e9d30d34331047e5d7d99398b649794ae61d
Alexandre B A Villares <abav@lugaralgum.com>
2019-12-06 14:09:24 -0200

    b6

---
+++ codigos/processing-python/base_estrelas_geral_b6/base_estrelas_geral_b6.pyde
@@ -0,0 +1,111 @@
+"""
+Vamos ler umas paradas
+"""
+from estrela import Estrela
+add_library('oscP5')  # precisa instalar no IDE do Processing oscP5!
+
+ins_override = -1  # -1 é o "normal" sem override
+# definie instrumentos que vão ser ouvidos
+instrumentos = (
+    "verdesol",
+    "laranjare",
+    "verdefa1",
+    # "verdefa2",
+    "vermelhodo1",
+    # "vermelhodo2",
+    "amarelomi",
+    "lilassi",
+)
+Estrela.full_screen = False # True para tela inteira e giro 90 graus da animação
+
+def settings():
+    size(800, 600)
+
+def setup():
+    """ Define área de desenho e popula lista de estrelas """
+    global dados, instrumentos, oscP5, novos_dados, estrelas
+    # fullScreen(1)  # testar se 1 vai para segundo monitor
+    this.surface.setResizable(True)
+    if not Estrela.full_screen:
+        this.surface.setSize(600, 800)
+    background(0)
+
+    # inicializa OSC
+    oscP5 = OscP5(this, 12000)
+    # inicialização das estruturas de dados
+    novos_dados = setup_dados()
+    dados = setup_dados()
+    print instrumentos
+    # cria uma estrela pra cada instrumento
+    estrelas = []  # lista de objetos
+    for i, _ in enumerate(instrumentos):
+        e = Estrela(0, i * height / len(instrumentos))
+        estrelas.append(e)
+
+def draw():
+    """ Limpa a tela, desenha e atualiza estrelas """
+    # background(0)  # fundo preto OU
+    # apagamento parcial para "rastro":
+    fill(0, 10)
+    noStroke()
+    rect(0, 0, width, height)
+
+    for i, estrela in enumerate(estrelas):
+        ins, tom, amp, cor = dados[instrumentos[i]]
+        if ins_override == 6:
+            ins = i
+        estrela.desenha(ins, cor, amp)
+        estrela.anda(tom)
+
+    for instrumento in instrumentos:
+        ___, tom, amp, cor = dados[instrumento]
+        nins, ntom, namp, ncor = novos_dados[instrumento]
+        if ins_override >= 0:
+            nins = ins_override
+        dados[instrumento] = (nins,                  # ins/modo/ins_override
+                              lerp(tom, ntom, .2),  # easing "suave"
+                              namp,                # sem easing
+                              cor, #(cor + ncor) / 2   # easing "rápido"
+                              )
+
+def oscEvent(oscMessage):
+    for instrumento in instrumentos:
+        if oscMessage and oscMessage.addrPattern() == "/" + instrumento:
+            ins = oscMessage.get(0).intValue() if oscMessage.get(0) else 0
+            tom = oscMessage.get(1).intValue() if oscMessage.get(1) else 0
+            amp = oscMessage.get(2).intValue() if oscMessage.get(2) else 0
+            cor = oscMessage.get(3).intValue() if oscMessage.get(3) else 0
+            novos_dados[instrumento] = (ins, tom, amp, cor)
+
+def setup_dados():
+    dados = dict()
+    for instrumento in instrumentos:
+        dados[instrumento] = (
+            0,  # ins/modo/ins_override
+            0,  # int(random(-24, 24)),
+            100,  # int(random(10, 150)),
+            0,  # int(random(360)),
+        )
+    return dados
+
+def keyPressed():
+    global ins_override
+    if str(key) in "01234567":
+        ins_override = int(key)
+        Estrela.ins_override = int(key)
+        print(ins_override)
+    if key == ' ':
+        ins_override = -1
+    if keyCode == SHIFT:
+        for instrumento in instrumentos:
+            print(instrumento, novos_dados[instrumento])
+    if key == 'f':
+        Estrela.full_screen = not Estrela.full_screen
+        if Estrela.full_screen:
+            this.surface.setSize(displayWidth, displayHeight)
+            this.surface.setLocation(0, 0)
+        else:
+            this.surface.setSize(600, 800)
+                    
+def mouseDragged():
+    this.surface.setLocation(mouseX, mouseY)
---
+++ codigos/processing-python/base_estrelas_geral_b6/estrela.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+from cores import *
+
+class Estrela():
+
+    """ Classe Estrela, cor sorteada, tamanho sorteado por default """
+    full_screen = False
+    ins_override = -1
+
+    def __init__(self, x, y, tam=100):
+        self.x, self.y = x, y
+        self.tamanho = tam
+        self.vy = -0.5
+
+    def desenha(self, ins, cor, amp):
+        """ Desenha polígono em torno das coordenadas do objeto """
+        raio1, raio2 = amp, amp / 4
+        self.tamanho = amp
+        colorMode(RGB)
+        cor_final = paleta(ins, cor)
+        stroke(cor_final, 150)
+        fill(cor_final)
+        strokeJoin(ROUND)
+        if Estrela.full_screen:
+            translate(width / 2, height / 2)
+            rotate(HALF_PI)
+            translate(-width / 2, -height / 2)
+        self.tamanho = amp
+        if ins in (0, 1):
+            strokeWeight(5)
+            noFill()
+            fill(0, 10)
+            if Estrela.ins_override == 6:
+                stroke(255,200)
+            estrela(self.x, self.y, 7, raio1, raio2)
+        if ins in (2, 3):
+            pushMatrix()
+            translate(self.x, self.y)
+            stroke(0)
+            if Estrela.ins_override == 6:
+                cor_final = 255
+            fill(cor_final, min(255, amp))
+            estrela(0, 0, 4, raio1, raio1 / 4)
+            rotate(QUARTER_PI)
+            noStroke()
+            s = 4 if ins == 5 else 5
+            cor2 = paleta(s, cor)
+            if Estrela.ins_override == 6:
+                cor2 = 255
+            fill(cor2, min(255, amp))                
+            estrela(0, 0, 4, raio1 * .8, raio1 / 4 * .8)
+            popMatrix()
+        if ins in (4, 5):
+            strokeWeight(2)
+            stroke(0)
+            pushMatrix()
+            translate(self.x, self.y)
+            rotate(radians(frameCount))
+            if Estrela.ins_override == 6:
+                cor_final = 255
+            fill(cor_final, 230 - min(230, amp))
+            s = 4 if ins == 5 else 5
+            stroke(paleta(s, cor))
+            if Estrela.ins_override == 6:
+                stroke(0, 100)
+            estrela(0, 0, 10, raio1, 50)
+            popMatrix()
+        if ins == 6:
+            fill(255, 100)
+            noStroke()
+            estrela(self.x, self.y, 10, raio1, 50)
+        if Estrela.full_screen:
+            translate(width / 2, height / 2)
+            rotate(-HALF_PI)
+            translate(-width / 2, -height / 2)
+
+    # def set_cores(ins, cor, s=None, f=None, sw=None):
+    #     if Estrela.ins_override == 6:
+    #         fill(255,200)
+    #         stroke(255,200)
+    # #     else:
+    # #         pass
+    #     if s is None: noStroke()
+    #     if f is None: noFill()
+
+
+    def anda(self, tom=None):
+        """ atualiza a posição do objeto e devolve do lado oposto se sair """
+        # if Estrela.full_screen:
+        #     w, h = height, width
+        # else:
+        w, h = width, height
+        if tom is not None:
+            self.x = map(tom, -24, 24, self.tamanho * 2, w - self.tamanho * 2)
+        # self.x += self.vx
+        self.vy = -1 * 40 / (tom + 40)
+        self.y += self.vy
+        tam = self.tamanho
+        if self.x > w + tam:
+            self.x = -tam
+        if self.y > h + tam:
+            self.y = -tam
+        if self.x < -tam:
+            self.x = w + tam
+        if self.y < -tam:
+            self.y = h + tam
+
+def estrela(cx, cy, pontas, raio1, raio2):
+    pontos = pontas * 2
+    parte = 360. / pontos
+    beginShape()  # comece a forma!
+    for p in range(pontos):  # para cada p
+        angulo = radians(p * parte)  # calcula angulo
+        if p % 2 == 0:  # se for par
+            raio = raio1
+        else:  # senão, se for impar
+            raio = raio2
+        x = cx + raio * sin(angulo)
+        y = cy + raio * cos(angulo)
+        vertex(x, y)  # vertex é um ponto
+    endShape(CLOSE)  # termina forma
